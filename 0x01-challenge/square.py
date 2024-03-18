1d4934d08fa4609e6729fd8d2bb5530894accb77
Babatunde Adeoti <babaolu98@gmail.com>
2024-03-18 08:14:22 +0100

    Fixing pycodestyle of square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" This module implements the square class """
+
 
 class square():
-    
+    """ The square class implementation """
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Initialization procedure """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +18,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Informal string representation """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
